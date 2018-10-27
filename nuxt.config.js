f2c50790c296a059398b7ff9446208d7fd29f640
yagisuke <yagisuke.0209@gmail.com>
2018-10-27 17:02:41 +0900

    router

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,3 @@
-  /* nuxt.config.js */
-  // `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
-  const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-    router: {
-      base: '/<repository-name>/'
-    }
-  } : {}
-  
 module.exports = {
   /*
   ** Headers of the page
@@ -24,7 +16,9 @@ module.exports = {
   plugins: [
     { src: '~plugins/pixi', ssr: false }
   ],
-  ...routerBase,
+  router: {
+    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/nuxt-pixi/' : '/'
+  },
   /*
   ** Customize the progress bar color
   */
