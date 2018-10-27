2c7944e130de6276fa97d24d95ff9a96eab97420
yagisuke <yagisuke.0209@gmail.com>
2018-10-27 16:38:55 +0900

    back-link

---
+++ components/molecules/link/back-link.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="back-link">
+    <nuxt-link to="/">TOPへもどる</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.back-link {
+  padding: 16px 0;
+}
+</style>
