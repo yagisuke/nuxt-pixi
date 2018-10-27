89d445e6a2345871aa3785800617eee659c45341
yagisuke <yagisuke.0209@gmail.com>
2018-10-27 15:29:48 +0900

    Container Pivot

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  plugins: [
+    { src: '~plugins/pixi', ssr: false }
+  ],
   /*
   ** Customize the progress bar color
   */
