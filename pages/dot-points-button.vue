0b1d285f6f60bf0229b4dbc7a495dc0bacf9c5cf
yagisuke <yagisuke.0209@gmail.com>
2018-11-06 00:02:53 +0900

    dotlink

---
+++ pages/dot-points-button.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <h1>Dot Points Button</h1>
+    <div class="background">
+      <a :class="{ 'clicked-link': clicked }" class="dot-points-button" title="click!" @click="clicked = !clicked">
+        <span class="dot" />
+        <span class="dot" />
+        <span class="dot" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      clicked: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.background {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 500px;
+  background-color: aquamarine;
+}
+
+.dot-points-button {
+  display: inline-flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: lightpink;
+  line-height: 0;
+  cursor: pointer;
+
+  > .dot {
+    display: inline-block;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: whitesmoke;
+  }
+}
+
+.clicked-link {
+  > .dot {
+    animation: pop-wave 1.3s linear infinite;
+  }
+
+  > .dot:nth-child(2) {
+    animation-delay: -1.1s;
+  }
+
+  > .dot:nth-child(3) {
+    animation-delay: -0.9s;
+  }
+}
+
+@keyframes pop-wave {
+  0%,
+  60%,
+  100% {
+    transform: initial;
+  }
+  30% {
+    transform: translateY(-4px) scale(1.4);
+    opacity: 0.25;
+  }
+}
+</style>
